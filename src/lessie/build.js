e4ea8cb64ce4bab1a1188fa33b95a47cbf191dea
Juraj Kirchheim <back2dos@googlemail.com>
2016-07-12 12:27:07 +0200

    Disable strict math.

--- src/lessie/build.js
+++ src/lessie/build.js
@@ -2,7 +2,7 @@ var cmd = process.argv[2],
     output = process.argv[3],
     input = process.argv[4];
 
-var result = require('child_process').spawnSync(cmd, ['-', output, '--no-color', '--strict-math=on', '--silent'], { input: input });
+var result = require('child_process').spawnSync(cmd, ['-', output, '--no-color'], { input: input });
 if (result.error) {
   require('fs').writeFileSync(output+'.errorlog', result.error.code);
   process.exit(500);
